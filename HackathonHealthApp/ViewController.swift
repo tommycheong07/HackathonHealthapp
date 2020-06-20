aba2518c69be3440e03012fffb1f77f4c22a72a2
thomascheong <tommycheong98@gmail.com>
2020-06-20 16:20:33 -0400

    Initial Commit

---
+++ HackathonHealthApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HackathonHealthApp
+//
+//  Created by Thomas Cheong on 6/20/20.
+//  Copyright © 2020 Thomas Cheong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

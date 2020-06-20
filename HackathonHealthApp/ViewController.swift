2583c1fc98e88d767058926e2499985d3492eefa
thomascheong <tommycheong98@gmail.com>
2020-06-20 18:26:21 -0400

    got some pages working

--- HackathonHealthApp/ViewController.swift
+++ HackathonHealthApp/ViewController.swift
@@ -10,27 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
-
-    @IBOutlet weak var totalAmount: UILabel!
-    
-    @IBOutlet weak var num1: UITextField!
-    @IBOutlet weak var num2: UITextField!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        totalAmount.text = ""
+        
     }
 
-    @IBAction func buttonCalculate(_ sender: Any) {
-        let first = Double(num1.text!)!
-        let second = Double(num2.text!)!
-        
-        let total = first + second
-        
-        totalAmount.text = "$\(total)"
-    }
     
 }
 

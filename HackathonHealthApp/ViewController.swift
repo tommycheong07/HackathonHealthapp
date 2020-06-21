e61b1221952bcb9a15a63a6fe83bc9e6ae2a81de
thomascheong <tommycheong98@gmail.com>
2020-06-20 20:02:40 -0400

    * rounded out buttons * added flow where if you log out there is no back button on top left

--- HackathonHealthApp/ViewController.swift
+++ HackathonHealthApp/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.navigationController?.viewControllers = [self]
         
     }
 
